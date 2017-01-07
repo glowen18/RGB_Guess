d697be6a19e127ae007a65a56353d5a9dcf21bb9
Gaye Lowenstein <glowen@hotmail.com>
2017-01-07 13:55:25 -0800

    fixed random colors to pick 3 or 6 based on easy or hard button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var colors =  generateRandomColors(6);
+var numSquares = 6;
+var colors =  generateRandomColors(numSquares);
 // Select all squares
 var squares = document.querySelectorAll(".square");
 //Hard codes picked color
@@ -17,16 +18,35 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function(){
   hardBtn.classList.remove("selected");
   easyBtn.classList.add("selected");
-})
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+    if(colors[i]){
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
 
 hardBtn.addEventListener("click", function(){
   easyBtn.classList.remove("selected");
   hardBtn.classList.add("selected");
-})
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++) {
+      squares[i].style.background = colors[i];
+      squares[i].style.display = "block";
+  }
+});
 
 resetButton.addEventListener("click", function(){
 //When click on button generates all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
 //Pick a new random color as picked color from array
   pickedColor = pickColor();
 //Change colorDisplay to match pickedColor
