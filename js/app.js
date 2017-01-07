c6d7dc3001337fb4133aabdaa3101f4d616531cb
Gaye Lowenstein <glowen@hotmail.com>
2017-01-07 11:58:26 -0800

    added hard-coded click event handler

--- js/app.js
+++ js/app.js
@@ -9,7 +9,29 @@ var colors =  [
 // Select all squares
 var squares = document.querySelectorAll(".square");
 
+//Hard codes picked color
+var pickedColor = colors[3];
+
+//Sets variable to pick a color by its ID
+var colorDisplay = document.getElementById("colorDisplay");
+
+//Displays color that is picked
+colorDisplay.textContent = pickedColor;
+
 //Loop thru all squares
 for(var i = 0; i < squares.length; i++) {
+  //Adds initial color to squares
   squares[i].style.background = colors[i];
+
+  //Adds click listener to squares
+  squares[i].addEventListener("click", function() {
+    //Grab color of clicked squares
+    var clickedColor = this.style.background;
+    //And compare color to pickedColor
+    if(clickedColor === pickedColor) {
+      alert("Correct!");
+    } else {
+      alert("Wrong!");
+    }
+  });
 }
