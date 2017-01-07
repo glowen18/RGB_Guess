e219adabf1ed4bfb8307741d3f8c57541e931484
Gaye Lowenstein <glowen@hotmail.com>
2017-01-07 12:27:37 -0800

    all squares change color to picked color

--- js/app.js
+++ js/app.js
@@ -29,11 +29,21 @@ for(var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.background;
 //And compare color to pickedColor
     if(clickedColor === pickedColor) {
-      messageDisplay.textContent = "Correct"
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
     } else {
 // When wrong square clicked, it disappears (fades to background color)
       this.style.background = "#233343";
-      messageDisplay.textContent = "Try Again"
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+//Loop thru all squares
+  for(var i = 0; i < squares.length; i++) {
+//Change each color to match pickedColor
+  squares[i].style.background = color;
+
+  }
 }
